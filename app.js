9f0bd03dbeae7b3395a3de8e1e685616a1a0e60a
Loyanne Cristine <loyanne.cristine@gmail.com>
2020-06-01 19:22:38 +0100

    added handlebars

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-
+const exphbs  = require('express-handlebars');
 const livereload = require("livereload");
 
 var liveReloadServer = livereload.createServer();
@@ -22,10 +22,18 @@ const app = express();
 const port = 3000;
 
 /*
+Global functions that are used by templates.
+*/
+const handlebars = exphbs.create();
+
+/*
 View engine setup
 */
 app.use(express.urlencoded({ extended: false }));                             // Define how form data should be encode.
 app.use(express.static(publicDirectory));
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
 
 /*
