11670e84406e3967780a37c76743b0cdeb885e52
Loyanne Cristine <loyanne.cristine@gmail.com>
2020-06-02 21:44:28 +0100

    indenting file

--- db/init_db.js
+++ db/init_db.js
@@ -10,17 +10,27 @@ db.query("CREATE DATABASE movieschema", function (err, res) {
     console.log("Schema/Database File Created");
 });
 
-db.query("USE movieschema", function(err,res){
-    if(err) throw err;
+db.query("USE movieschema", function (err, res) {
+    if (err) throw err;
     console.log("Using database movieschema");
 });
 
-db.query("CREATE TABLE movieschema.users (userid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,surname VARCHAR(255),firstname VARCHAR(255),emailaddress VARCHAR(255),password VARCHAR(255))", function(err,res){
-    if(err) throw err;
+db.query("CREATE TABLE movieschema.users (" +
+    "userid INT NOT NULL AUTO_INCREMENT PRIMARY KEY," +
+    "surname VARCHAR(255)," +
+    "firstname VARCHAR(255)," +
+    "emailaddress VARCHAR(255)," +
+    "password VARCHAR(255))", function (err, res) {
+    if (err) throw err;
     console.log("Table Created");
 });
 
-db.query("CREATE TABLE movieschema.userrating (ratingid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,userid INT NOT NULL,user_rating INT,movieid INT ,FOREIGN KEY(userid) REFERENCES users(userid))", function(err,res){
-    if(err) throw err;
+db.query("CREATE TABLE movieschema.userrating (" +
+    "ratingid INT NOT NULL AUTO_INCREMENT PRIMARY KEY," +
+    "userid INT NOT NULL," +
+    "user_rating INT," +
+    "movieid INT," +
+    "FOREIGN KEY(userid) REFERENCES users(userid))", function (err, res) {
+    if (err) throw err;
     console.log("Table Created");
 });
