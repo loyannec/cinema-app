93f56ad20c8fb463f9921869b96d3676c8ae4602
Loyanne Cristine <loyanne.cristine@gmail.com>
2020-06-02 21:09:14 +0100

    update file

--- plugins/express.js
+++ plugins/express.js
@@ -1,8 +1,7 @@
 module.exports = (app) => {
     const express = require('express');
-    var cookieParser = require('cookie-parser');
+
     app.use(express.json());
-    app.use(express.urlencoded({ extended: false }));  
-    app.use(cookieParser());                           // Define how form data should be encode.
+    app.use(express.urlencoded({ extended: false }));                           // Define how form data should be encode.
     app.use(express.static("Ajax"));
 };
