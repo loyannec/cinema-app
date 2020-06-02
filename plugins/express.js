259a7ad454fe97e6cb0325550d07a9c0b64ef6b0
Loyanne Cristine <loyanne.cristine@gmail.com>
2020-06-02 12:21:38 +0100

    refactoring files

---
+++ plugins/express.js
@@ -0,0 +1,7 @@
+module.exports = (app) => {
+    const express = require('express');
+
+    app.use(express.json());
+    app.use(express.urlencoded());                             // Define how form data should be encode.
+    app.use(express.static("Ajax"));
+};
