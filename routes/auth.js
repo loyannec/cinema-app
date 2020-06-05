91b7fec36c52b55f8476940ef890a892f7e97a84
Loyanne Cristine <loyanne.cristine@gmail.com>
2020-06-05 17:33:26 +0100

    indentation

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,6 @@ const JWT= require('jsonwebtoken');
 
 const TIME_LIMIT = 1000*60;
 const {
-  
   SESSION_EXPIRY = TIME_LIMIT,
   SESSION_NAME='sid',
   SESSION_SECRET='secret'
@@ -17,14 +16,14 @@ const crypto = require("crypto");
 // we store the tokens in-memory for simplicity's sake
 // in production we'd make them persistent
 
-const auth= {
-  setAuthToken:(req,res,next)=>{
+const auth = {
+  setAuthToken:(req,res,next) => {
     console.log("Inside setAuthToken method")
-    const userID=req.userID;
-    console.log("!!!!!!!"+userID)
-    const token=JWT.sign({userID}, SESSION_SECRET);
-    console.log("Token"+token)
-    res.cookie('access_token',token);
+    const userID = req.userID;
+    console.log("!!!!!!!" + userID)
+    const token = JWT.sign({userID}, SESSION_SECRET);
+    console.log("Token" + token)
+    res.cookie('access_token', token);
     next();
   },
 
@@ -33,12 +32,12 @@ const auth= {
     console.log("Token in verify"+token)
     if(token)
     {
-        const decoded=JWT.verify(token,SESSION_SECRET);
-        console.log("USER Id"+decoded+decoded.userID);
+        const decoded = JWT.verify(token, SESSION_SECRET);
+        console.log("USER Id" + decoded + decoded.userID);
       //if(decoded.userID!=='undefined'||decoded.userID!=='null'){
         console.log("Token is present ")
-        req.id=decoded.userID;
-        console.log("ID is present "+req.id);
+        req.id = decoded.userID;
+        console.log("ID is present " + req.id);
     }
     next();
   },
@@ -47,15 +46,11 @@ const auth= {
     console.log("Inside unsettoken method")
     res.clearCookie('access_token');
     res.render('login',
-    { 
+    {
       message: 'Logged out !Please Login to Continue!',
       messageClass: 'alert-success'
     });
   },
-  
 };
-module.exports=auth;
 
- 
-
-
+module.exports = auth;
