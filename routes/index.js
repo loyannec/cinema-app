d592abd918f3a9e7c88ce9a8327c10a443161954
Loyanne Cristine <loyanne.cristine@gmail.com>
2020-06-05 17:34:52 +0100

    indentation

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,39 @@
 module.exports = (app) => {
 const axios = require('axios');
 var express = require('express');
- 
-//var router = express.Router();
-const {getHomePage,getLoginPage,submitUserRating,verifyLogin,getMoviePage,getRegisterPage,submitUserRegistration}= require('../controllers/moviescontroller');
-const {verifyAuthToken,setAuthToken,unsetAuthToken}= require('../routes/auth');
+
+const {getHomePage,
+        searchMovie,
+        getLoginPage,
+        submitUserRating,
+        verifyLogin,
+        getMoviePage,
+        getRegisterPage,
+        submitUserRegistration} = require('../controllers/moviescontroller');
+const {verifyAuthToken,
+        setAuthToken,
+        unsetAuthToken} = require('../routes/auth');
 
 /* GET home page. */
-app.get('/',verifyAuthToken, getHomePage); 
+app.get('/',verifyAuthToken, getHomePage);
 //router.get('/',getHomePage);
 
-app.get('/searchmovie',verifyAuthToken, getHomePage); 
+app.get('/searchmovie',verifyAuthToken, searchMovie);
+
 /*Display Home page*/
-app.get('/login', getLoginPage); 
+app.get('/login', getLoginPage);
 //router.get('/login', getLoginPage);
 
 /*Display MoviesHome page*/
-app.post('/login',[verifyLogin,setAuthToken],getHomePage); 
+app.post('/login',[verifyLogin,setAuthToken],getHomePage);
 //router.post('/login',[verifyLogin,setAuthToken],getHomePage);
 
 /*Display respective Movie Page*/
-app.get('/movie/:id', verifyAuthToken,getMoviePage); 
+app.get('/movie/:id', verifyAuthToken,getMoviePage);
 //router.get('/movie/:id',verifyAuthToken,getMoviePage);
 
-
-
 /*Display respective Movie Page*/
-app.post('/userrating',verifyAuthToken,submitUserRating); 
+app.post('/userrating',verifyAuthToken,submitUserRating);
 //router.post('/userrating',submitUserRating);
 
 /*Display Register page*/
@@ -34,10 +41,9 @@ app.get('/register',getRegisterPage);
 //router.get('/register',getRegisterPage);
 
 /*Submits User details*/
-app.post('/register',submitUserRegistration); 
+app.post('/register',submitUserRegistration);
 //router.post('/register',submitUserRegistration);
 
-/* Log out user and display login page. */  
+/* Log out user and display login page. */
 app.get('/logout', verifyAuthToken,unsetAuthToken);
-
 };
